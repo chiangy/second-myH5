561e99707da3c7e30ecd4edbb88fbbf0f02bc2ec
win-jy <chiangying@163.com>
2019-05-31 15:24:04 +0800

    js document

---
+++ 002console/js/alert.js
@@ -0,0 +1,15 @@
+function woDeTaoLu () {
+    alert("你猜一会儿会发生什么？");
+    alert("再等等");
+    alert("也许有奇迹");
+}
+
+alert("你怎么打开了这个网页");
+woDeTaoLu();
+alert("你相信奇迹吗？");
+alert("让我来向你提个问题");
+woDeTaoLu();
+alert("果然没有奇迹么？");
+alert("我们就这样放弃么？");
+woDeTaoLu();
+alert("这是最后一句了.88");
